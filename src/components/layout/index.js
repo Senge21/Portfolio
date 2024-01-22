d47f4110e94b812b1027c64ce7f496eb70f3c23a
Senge21 <Ssenge1515@gmail.com>
2024-01-22 13:29:44 +0700

    1-22 home

---
+++ src/components/layout/index.js
@@ -0,0 +1,10 @@
+export * from "./Header";
+export * from "./Footer";
+export * from "./About";
+export * from "./Experience";
+export * from "./GetInTouch";
+export * from "./Profile";
+export * from "./Skills";
+export * from "./Work";
+
+// export const Js = require("");
---
+++ src/pages/index.jsx
@@ -0,0 +1,26 @@
+import {
+  Header,
+  Profile,
+  About,
+  Skills,
+  Experience,
+  Work,
+  GetInTouch,
+  Footer,
+} from "@/components/layout";
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className="">
+        <Header />
+        <Profile />
+        <About />
+        <Skills />
+        <Experience />
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
